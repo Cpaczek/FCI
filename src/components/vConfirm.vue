c837250d447ee9347d4619fc610cf9719b6b53e1
Cameron <github@strixdesigns.com>
2019-07-09 03:00:54 -0700

    Make Modals in to Vue Single Files Components

---
+++ src/components/vConfirm.vue
@@ -0,0 +1,187 @@
+<template>
+  <div>
+    <div class="modal is-active" v-show="showModal">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div v-if="loading" style="display:flex;">
+          <div class="lds-circle"><div></div></div>
+        </div>
+        <table class="table is-bordered">
+          <thead>
+          <tr>
+            <th>Appointment Time</th>
+            <th>Full Name</th>
+            <th>Check In</th>
+          </tr>
+          </thead>
+
+          <tbody>
+          <tr v-for="appt in appts">
+            <td>{{appt.Time}}</td>
+            <td>{{appt.First}} {{appt.Last}}</td>
+            <td v-if="appt.conf < 269 || appt.conf > 268"><a class="button" @click="confirm(appt.apt, appt.patid)">Check In</a>
+            </td>
+            <td v-else><a class="button" disabled>Checked In</a></td>
+          </tr>
+          </tbody>
+        </table>
+
+        <div class="has-text-centered">
+          <a href="#" @click="clear()" class="button">Cancel</a>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</template>
+<script>
+  import EventBus from '../event-bus.js';
+  import Vue from 'vue';
+  import App from '../App.vue';
+  import {store} from '../store';
+  import axios from 'axios';
+  import Swal from 'sweetalert2'
+  export default {
+    store,
+    data: () => {
+      return {
+        appts: [],
+        loading: false,
+        showModal: false,
+
+      }
+
+
+    },
+    mounted () {
+      EventBus.$on('UpdateAppts', (payload) => {
+          this.loading = true;
+        console.log("listened")
+        console.log(payload + " - payload")
+        this.showModal = true;
+
+
+        let _this = this;
+        axios.get('http://10.12.0.1:3000/appointments')
+          .then(function (response) {
+            console.log(response);
+            for (let i = 0; i < response.data.length; i++) {
+              let obj = response.data[i];
+              console.log('looping');
+              console.log(obj.LName.toLowerCase())
+              if (obj.LName.toLowerCase() === payload.toLowerCase()) {
+                console.log("FName" + obj.FName);
+                console.log("parsed" + Date.parse(obj.AptDateTime))
+                _this.appts.push({
+                  "First": obj.FName,
+                  "Last": obj.LName,
+                  "Time": _this.getTimeFromDate(new Date(Date.parse(obj.AptDateTime))),
+                  "apt": obj.aptnum,
+                  "conf": obj.confirmed,
+                  "patid": obj.patnum
+                });
+
+              }
+
+              console.log(obj);
+            }
+            if (_this.appts.length < 1) {
+              Swal.fire("No appointments!", "No appointments were found for the given last name. Please check your spelling and try again.", "error",);
+            } else {
+              _this.isActive = true;
+            }
+            _this.loading=false;
+
+
+          })
+          .catch(function (error) {
+            Swal.fire('Oops...', 'Something went wrong! Please talk to receptionist ', 'error')
+            console.log(error);
+          })
+          .finally(function () {
+          });
+
+      })
+    },
+    methods: {
+      confirm(aptnum, patnum){
+        this.$store.commit('setPatId', patnum)
+        let _this = this;
+        axios.post('http://10.12.0.1:3000/appointments/confirm/' + aptnum)
+          .then(function (response) {
+            // handle success
+            console.log(response);
+            let payload = patnum;
+            EventBus.$emit('FillInfo', payload)
+            _this.showModal = false;
+            _this.appts = [];
+
+
+          })
+          .catch(function (error) {
+            // handle error
+            console.log(error);
+            Swal.fire('Oops...', 'Something went wrong! Please talk to receptionist ', 'error')
+          })
+          .finally(function () {
+            // always executed
+          });
+      },
+      clear(){
+        EventBus.$emit('clear')
+        this.showModal=false;
+      },
+
+
+
+    getTimeFromDate(date) {
+      let hours = date.getHours();
+      let minutes = date.getMinutes();
+      let ampm = hours >= 12 ? 'pm' : 'am';
+      hours = hours % 12;
+      hours = hours ? hours : 12; // the hour '0' should be '12'
+      minutes = minutes < 10 ? '0' + minutes : minutes;
+      let strTime = hours + ':' + minutes + ' ' + ampm;
+      return strTime;
+    }
+  }
+
+
+  }
+</script>
+<style>
+  .lds-circle {
+    display: inline-block;
+    transform: translateZ(1px);
+    margin: auto
+  }
+  .lds-circle > div {
+    display: inline-block;
+    width: 51px;
+    height: 51px;
+    margin: 6px;
+    border-radius: 50%;
+    background: #000000;
+    animation: lds-circle 2.4s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+  }
+  @keyframes lds-circle {
+    0%, 100% {
+      animation-timing-function: cubic-bezier(0.5, 0, 1, 0.5);
+    }
+    0% {
+      transform: rotateY(0deg);
+    }
+    50% {
+      transform: rotateY(1800deg);
+      animation-timing-function: cubic-bezier(0, 0.5, 0.5, 1);
+    }
+    100% {
+      transform: rotateY(3600deg);
+    }
+  }
+
+</style>
+
+
+
---
+++ src/components/vUpdate.vue
@@ -0,0 +1,231 @@
+<template>
+  <div>
+    <div class="modal is-active" v-show="showModal">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="container is-center has-text-center">
+
+          <h1>Update Info</h1>
+
+          <div class="columns">
+            <div class="column is-one-third">
+              <label>Preferred Contact Method</label>
+              <div class="field">
+                <div class="control">
+
+                  <div class="select is-info">
+                    <select v-model="contactSelected">
+                      <option v-for="option in contactOptions" v-bind:value="option.value">
+                        {{ option.text }}
+                      </option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="column is-one-third">
+              <label>Preferred Confirm Method</label>
+              <div class="field">
+                <div class="control">
+                  <div class="select is-info">
+                    <select v-model="confirmSelected">
+                      <option v-for="option in contactOptions" v-bind:value="option.value">
+                        {{ option.text }}
+                      </option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="column is-one-third">
+              <label>Preferred Recall Method</label>
+              <div class="field">
+                <div class="control">
+                  <div class="select is-info">
+                    <select v-model="recallSelected">
+                      <option v-for="option in contactOptions" v-bind:value="option.value">
+                        {{ option.text }}
+                      </option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="field">
+            <div class="control">
+              <label>Cell Phone</label>
+              <input placeholder="(226)555-5555" v-model="cell" type="text" class="input">
+              <label>Work Phone</label>
+              <input placeholder="(226)555-5555" v-model="work" type="text" class="input">
+              <label>Home Phone</label>
+              <input placeholder="(226)555-5555" v-model="home" type="text" class="input">
+              <label>Email</label>
+              <input type="text" class="input" v-model="email">
+              <div class="has-text-centered">
+                <p>If you need to change your address please see the receptionist </p>
+                <i>
+                  <h1>{{address}}</h1>
+                </i>
+
+                <br>
+                <i>
+                  <h1>{{address2}}</h1>
+                </i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="has-text-centered">
+          <a href="#" class="button" style="margin-top: 15px;" @click="updateInfo">Update Info</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import EventBus from '../event-bus.js';
+  import Vue from 'vue';
+  import App from '../App.vue';
+  import {store} from '../store';
+  import axios from 'axios';
+  import Swal from 'sweetalert2'
+  export default {
+    store,
+    props: {},
+    data: () => {
+      return {
+        showModal: false,
+
+        contactOptions: [{text: "None", value: 0}, {text: "Do not call", value: 1},
+          {text: "Home Phone", value: 2},
+          {text: "Work Phone", value: 3},
+          {text: "Wireless Phone", value: 4},
+          {text: "Email", value: 5},
+          {text: "Custom", value: 6},
+          {text: "Mail", value: 7},
+          {text: "Text", value: 8}],
+
+        contactSelected: 3,
+        confirmSelected: 3,
+        recallSelected: 3,
+
+        home: "",
+        work: "",
+        cell: "",
+        email: "",
+        address: "",
+        address2: "",
+
+        currentPat: [],
+
+        patnum: 0,
+      }
+
+    },
+    computed: {
+      getPatId(){
+        return this.$store.getters.getPatId
+      }
+    },
+    //Populate Modal With Data
+    mounted () {
+      EventBus.$on('FillInfo', (payload) => {
+        this.showModal = true;
+        this.patnum = payload;
+
+        let _this = this;
+        axios.get('http://10.12.0.1:3000/patients/' + this.patnum)
+          .then(function (response) {
+            console.log(response + "updateFill")
+            _this.currentPat.push({
+              "PreferConfirmMethod": response.data[0].PreferConfirmMethod,
+              "PreferContactMethod": response.data[0].PreferContactMethod,
+              "PreferRecallMethod": response.data[0].PreferRecallMethod,
+              "WirelessPhone": response.data[0].WirelessPhone,
+              "WkPhone": response.data[0].WkPhone,
+              "HmPhone": response.data[0].HmPhone,
+              "Email": response.data[0].Email,
+              "Address": response.data[0].Address,
+              "Address2": response.data[0].Address2,
+            });
+            console.log("We Have Gotten Here")
+            _this.confirmSelected = _this.currentPat[0].PreferConfirmMethod;
+            _this.recallSelected = _this.currentPat[0].PreferRecallMethod;
+            _this.contactSelected = _this.currentPat[0].PreferContactMethod;
+            _this.cell = _this.currentPat[0].WirelessPhone;
+            _this.home = _this.currentPat[0].HmPhone;
+            _this.work = _this.currentPat[0].WkPhone;
+            _this.email = _this.currentPat[0].Email;
+            _this.address = _this.currentPat[0].Address;
+            _this.address2 = _this.currentPat[0].Address2;
+          })
+          .catch(function (error) {
+            Swal.fire('Oops...', 'Something went wrong! Please talk to receptionist ', 'error')
+            return;
+          })
+          .finally(function () {
+
+          });
+      });
+    },
+
+    methods: {
+      updateInfo(){
+        let phoneReg = /^[\\(]{0,1}([0-9]){3}[\\)]{0,1}[ ]?([^0-1]){1}([0-9]){2}[ ]?[-]?[ ]?([0-9]){4}[ ]*((x){0,1}([0-9]){1,5}){0,1}$/
+        if (!this.cell.match(phoneReg) && !this.work.match(phoneReg) && !this.home.match(phoneReg)) {
+          Swal.fire('Oops...', 'Invalid or No phone number (format is (xxx)xxx-xxxx', 'error')
+          return
+        }
+        if ((this.confirmSelected === 6 || this.confirmSelected === 7 || this.confirmSelected === 1) && !(this.currentPat[0].PreferConfirmMethod === 6 || this.currentPat[0].PreferConfirmMethod === 7 || this.currentPat[0].PreferConfirmMethod === 1)) {
+          Swal.fire('Oops...', 'You can not use that as a Confirm Contact Method', 'error')
+          return
+        }
+        if ((this.recallSelected === 6 || this.recallSelected === 7 || this.recallSelected === 1) && !(this.currentPat[0].PreferRecallMethod === 6 || this.currentPat[0].PreferRecallMethod === 7 || this.currentPat[0].PreferRecallMethod === 1)) {
+          Swal.fire('Oops...', 'You can not use that as a Recall Contact Method', 'error')
+          return
+        }
+        if ((this.contactSelected === 6 || this.contactSelected === 7 || this.contactSelected === 1) && !(this.currentPat[0].PreferContactMethod === 6 || this.currentPat[0].PreferContactMethod === 7 || this.currentPat[0].PreferContactMethod === 1)) {
+          Swal.fire('Oops...', 'You can not use that as a Contact Method', 'error')
+          return
+        }
+        this.showModal = false;
+        let _this = this;
+        axios.put('http://10.12.0.1:3000/patients/' + _this.patnum, {
+          PreferConfirmMethod: _this.confirmSelected,
+          PreferRecallMethod: _this.recallSelected,
+          PreferContactMethod: _this.contactSelected,
+          WirelessPhone: _this.cell,
+          HmPhone: _this.home,
+          WkPhone: _this.work,
+          Email: _this.email
+
+        })
+          .then(function (response) {
+            console.log(response)
+            console.log(_this.patnum)
+            _this.showModal = false;
+            _this.$store.commit('setPatId', 0)
+            Swal.fire(
+              'Confirmed',
+              'Thank you, you will be called shortly',
+              'success'
+            );
+            EventBus.$emit('clear')
+          })
+          .catch(function (error) {
+            Swal.fire('Oops...', 'Something went wrong! Please talk to receptionist 1', 'error')
+            EventBus.$emit('clear')
+
+          })
+          .finally(function () {
+
+          });
+
+      }
+    }
+  }
+
+
+</script>
